ed295f549493a7d4d4578809e9693c3187dfb574
Pedro dos Santos Pereira <pedrodkp@gmail.com>
2018-01-13 21:08:19 -0200

    progress bar

--- app/src/main/java/com/upiara/sals_android/MainActivity.java
+++ app/src/main/java/com/upiara/sals_android/MainActivity.java
@@ -1,40 +1,68 @@
 package com.upiara.sals_android;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int mUIFlag =
-            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE;
+    WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // modo imersão
-        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+        hideSystemUI();
 
-        WebView webView = (WebView) findViewById(R.id.activity_main_webview);
-        webView.setWebViewClient(new WebViewClient());
+        webView = (WebView) findViewById(R.id.activity_main_webview);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        webView.setWebViewClient(new myWebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://sals.upiara.com");
+    }
+
+    public class myWebViewClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url)
+        {
+            webView.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(view.GONE);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        // modo imersão
-        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+        hideSystemUI();
     }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
 }
